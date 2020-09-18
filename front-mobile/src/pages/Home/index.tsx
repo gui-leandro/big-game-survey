71d1e7ec60e7836766fe092449ad0c3c41f15f71
Guilherme <guilherme.gl1997@gmail.com>
2020-09-18 15:15:20 -0300

    feat: add home page mobile

---
+++ front-mobile/src/pages/Home/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { FontAwesome5 as Icon } from '@expo/vector-icons';
+import { View, Text, StyleSheet, Image, Alert } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+const Home: React.FC = () => {
+
+  const handleOnPress = () => {
+    Alert.alert('clicou')
+  }
+
+  return (
+    <>
+      <View style={ styles.container }>
+        <Image 
+          source={require('../../assets/gamer.png')}
+          style={styles.gamerImage}
+        />
+        <Text style={styles.title}>Vote agora!</Text>
+        <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleOnPress}>
+          <Text style={styles.buttonText}>
+            COLETAR DADOS
+          </Text>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="chevron-right" color="#fff" size={25} />
+            </Text>
+          </View>
+        </RectButton>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:  '15%',
+    backgroundColor: '#0B1F34',
+    alignItems: 'center',
+  },
+  gamerImage: {
+    width: 224,
+    height: 208,
+  },
+  title: {
+    color: '#00D4FF',
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginTop: 21,
+    fontFamily: "Play_700Bold",
+  },
+  subTitle: {
+    color: '#ED7947',
+    fontSize: 21,
+    marginTop: 15,
+    fontFamily: "Play_400Regular",
+  },
+  footer: {
+    marginTop: '10%',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#00D4FF',
+    flexDirection: 'row',
+    borderRadius: 10
+  },
+  buttonIcon: {
+    backgroundColor: '#ED7947',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    borderBottomRightRadius: 10,
+    borderTopRightRadius: 10
+  },
+  buttonText: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontFamily: "Play_700Bold",
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#0B1F34',
+  }
+});
+
+export default Home;
